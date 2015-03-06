629c87e147b8e0cecdf34b5d8802a23f7f60a639
Adam Wells <adamwells.dev@gmail.com>
2015-03-06 13:41:09 -0800

    Complete all up to insert for SQL object

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,10 +5,27 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    self_with_columns = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+
+    self_with_columns.first.map(&:to_sym)
   end
 
   def self.finalize!
+    columns.each do |column|
+      define_method("#{column}") do
+        @attributes[column]
+      end
+
+      define_method("#{column}=") do |value|
+        @attributes ||= {}
+        @attributes[column] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
@@ -20,31 +37,64 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    object_hashes = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+
+    self.parse_all(object_hashes)
   end
 
   def self.parse_all(results)
-    # ...
+    sql_objects = []
+    results.each do |result|
+      sql_objects << self.new(result)
+    end
+    sql_objects
   end
 
   def self.find(id)
-    # ...
+    object_hash = DBConnection.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        id = ?
+    SQL
+
+    self.parse_all(object_hash).first
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |key, value|
+      unless self.class.columns.include?(key.to_sym)
+        raise "unknown attribute '#{key}'"
+      end
+      self.send "#{key}=", value
+    end
   end
 
   def attributes
-    # ...
+    @attributes
   end
 
   def attribute_values
-    # ...
+    @attributes.values
   end
 
   def insert
-    # ...
+    sanitize = (["?"] * attribute_values.size).join(', ')
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{@attributes.keys.join(', ')})
+      VALUES
+        (#{sanitize})
+
+    SQL
   end
 
   def update
