60d139bdc1192ab4987729675fcaf3f90dc626f9
Adam Wells <adamwells.dev@gmail.com>
2015-03-06 14:49:56 -0800

    Add #where for searchables

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,25 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    conditions = []
+    params.keys.each do |key|
+      conditions << "#{key} = ?"
+    end
+
+    hash_values = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{conditions.join(' AND ')}
+    SQL
+
+    self.parse_all(hash_values)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
